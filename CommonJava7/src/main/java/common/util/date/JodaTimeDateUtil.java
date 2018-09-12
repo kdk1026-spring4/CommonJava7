6e9757ce58adf5fcdc82e7a61ba8899fc74e3734
김대광 <@DESKTOP-FSRD4MA>
2018-09-12 21:42:16 +0900

    정리

--- CommonJava7/src/main/java/common/util/date/JodaTimeDateUtil.java
+++ CommonJava7/src/main/java/common/util/date/JodaTimeDateUtil.java
@@ -20,7 +20,7 @@ public class JodaTimeDateUtil {
 	}
 	
 	private static final String YYYYMMDD = "yyyyMMdd";
-	private static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+	public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
 
 	/**
 	 * 현재 날짜 및 시간 반환
@@ -567,12 +567,13 @@ public class JodaTimeDateUtil {
 		}
 		
 		/**
-		 * milliseconds to DateTime
+		 * milliseconds to String
 		 * @param mills
 		 * @return
 		 */
-		public static DateTime millsToDateTime(long mills) {
-			return new DateTime(mills);
+		public static String millsToString(long mills) {
+			DateTime dateTime = new DateTime(mills); 
+			return dateTime.toString(YYYYMMDDHHMMSS);
 		}
 		
 		/**
@@ -587,12 +588,12 @@ public class JodaTimeDateUtil {
 		}
 		
 		/**
-		 * timestamp to DateTime
+		 * timestamp to String
 		 * @param timestamp (sec)
 		 * @return
 		 */
-		public static DateTime timestampToDateTime(long timestamp) {
-			return millsToDateTime(timestamp * 1000);
+		public static String timestampToString(long timestamp) {
+			return millsToString(timestamp * 1000);
 		}
 		
 	}
